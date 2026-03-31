49ebaa259f16d06dd603a8e70960d0702528331d
Hubert Lin <linhub15@users.noreply.github.com>
2026-03-30 19:27:05 -0600

    content: remove WEM dental

--- src/index.tsx
+++ src/index.tsx
@@ -52,17 +52,6 @@ export default (data: Lume.Data, helpers: Lume.Helpers) => (
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
             />
-            <iframe
-              class="rounded-2xl"
-              title="WEM Dental"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2371.928809797641!2d-113.63089968707533!3d53.52332827222581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a0205a10000001%3A0x8b5abdb29454c394!2sWEM%20Dental!5e0!3m2!1sen!2sca!4v1744304954372!5m2!1sen!2sca"
-              width="100%"
-              height="300"
-              style="border:0;"
-              allowFullScreen={false}
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            />
           </div>
         </div>
       </section>
